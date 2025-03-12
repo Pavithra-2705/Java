d9029965419464de6cafc1d14411a9dd3716cc7e
Pavithra-2705 <pavithramuthuraj343@gmail.com>
2025-03-12 15:35:37 +0530

    Heyy

---
+++ demo4.java
@@ -0,0 +1,7 @@
+public class demo4{
+    public static void main(String[] args){
+        String name1="Pavithra";
+        String name2="Muthuraj";
+        System.out.println(name1+name2);
+    }
+}
