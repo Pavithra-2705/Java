18d70fa39e00bda3e10e1fd5eb41efbfaa9313ff
Pavithra-2705 <pavithramuthuraj343@gmail.com>
2025-03-12 15:47:09 +0530

    Hi buddy

---
+++ demo5.java
@@ -0,0 +1,7 @@
+public class demo5{
+    public static void main(String[] args) {
+        String name1="Pavithra";
+        String name2="Muthuraj";
+        System.out.println(name1.concat(name2));
+    }
+}
