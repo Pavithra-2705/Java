8358259fce21bdaba2a919c7fb334415b033882f
Pavithra-2705 <pavithramuthuraj343@gmail.com>
2025-03-12 15:15:29 +0530

    Hey whatsup!!

---
+++ demo2.java
@@ -0,0 +1,6 @@
+public class demo2{
+    public static void main(String[] args){
+        System.out.print("Hello dude !");//To print hello
+        System.out.print("how r u ?");//To print how r u
+    }
+}
