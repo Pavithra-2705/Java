576472c5426a4e2f78893e56eb68e9c0a1d75517
Pavithra-2705 <pavithramuthuraj343@gmail.com>
2025-03-13 14:38:20 +0530

    If-else statement

---
+++ demo17.java
@@ -0,0 +1,11 @@
+public class demo17{
+    public static void main(String[] args){
+        int age=19;
+        if(age>=18){
+            System.out.println("You are eligible for voting !");
+        }
+        else{
+            System.out.println("You are not eligible for voting !");
+        }
+    }
+}
