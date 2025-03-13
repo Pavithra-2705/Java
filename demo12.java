93f1fd9c3883a133182cc18735dae25ac3a26b97
Pavithra-2705 <pavithramuthuraj343@gmail.com>
2025-03-13 12:00:23 +0530

    Java Logical Operator

---
+++ demo12.java
@@ -0,0 +1,9 @@
+public class demo12{
+    public static void main(String[] args){
+        int a=10;
+        int b=15;
+        System.out.println(a!=b&&a<b);
+        System.out.println(a<b&&a>b);
+        System.out.println(a<b||a>b);
+    }
+}
