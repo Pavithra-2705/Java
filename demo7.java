c751f8f0e607ed2722650f8210d8a0bb801d3dbe
Pavithra-2705 <pavithramuthuraj343@gmail.com>
2025-03-13 10:23:42 +0530

    Student Details

---
+++ demo7.java
@@ -0,0 +1,12 @@
+public class demo7 {
+    public static void main(String[] args) {
+        String name="Pavithra";
+        String deptandcls ="CSEB";
+        int mark=95;
+        char grade='A';
+        System.out.println("Enter the name of the student :"+name);
+        System.out.println("Enter the Class :"+deptandcls);
+        System.out.println("Enter the mark of the student :"+mark); 
+        System.out.println("Enter the grade :"+grade);
+    }
+}
