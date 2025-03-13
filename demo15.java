651c996489617aef395ebfb4ccec65ab8db066c2
Pavithra-2705 <pavithramuthuraj343@gmail.com>
2025-03-13 14:28:30 +0530

    Length of a string

---
+++ demo15.java
@@ -0,0 +1,9 @@
+public class demo15{
+    public static void main(String[] args){
+        String a=("kittens");
+        String b=("I love kittens!!!");
+        System.out.println("Lenght of the string :"+a.length());
+        System.out.println("Lenght of the string :"+b.length());
+        
+    }
+}    
