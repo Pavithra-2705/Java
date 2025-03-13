79509b7356c006e12c5968aa0f89675302bed412
Pavithra-2705 <pavithramuthuraj343@gmail.com>
2025-03-13 11:48:19 +0530

    Java Operators II

---
+++ demo11.java
@@ -0,0 +1,14 @@
+public class demo11{
+    public static void main(String[] args){
+        String name1="Pavithra";
+        String name2="Pavithra";
+        String name3="Shaktivel";
+        int a=10;
+        int b=15;
+        System.out.println(name1==name2);
+        System.out.println(name1==name3);
+        System.out.println(name1!=name3);
+        System.out.println(a<b);
+        System.out.println(a>b);
+    }
+}
