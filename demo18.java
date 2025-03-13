d39b31d643a237c046c9ef71c35457b2d601d2cc
Pavithra-2705 <pavithramuthuraj343@gmail.com>
2025-03-13 15:35:33 +0530

    Nested if statement

---
+++ demo18.java
@@ -0,0 +1,17 @@
+public class demo18{
+    public static void main(String[] args) {
+        int A=10;
+        int B=20;
+        if(A>B){
+            System.out.println("\'A\' is greater than B");
+        }
+        else{
+            if(B>A){
+            System.out.println("\'B\' is greater than A");
+        }    
+
+        }    
+            }
+        }
+
+    
