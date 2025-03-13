6d9ae5f1c1a069b2e531adbc7542c35fa29ff14f
Pavithra-2705 <pavithramuthuraj343@gmail.com>
2025-03-13 11:11:18 +0530

    Java Operators

---
+++ demo10.java
@@ -0,0 +1,9 @@
+public class demo10{
+    public static void main(String[] args){
+        int a=20;
+        int b=24;
+        System.out.println(a+b);
+        System.out.println(a*b);
+
+    }
+}
