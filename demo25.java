ad03ed8b0d034ef7a8fbd896cb0087b707190adf
Pavithra-2705 <pavithramuthuraj343@gmail.com>
2025-03-14 13:17:52 +0530

    Java Method II

---
+++ demo25.java
@@ -0,0 +1,10 @@
+public class demo25{
+    static void mul(){
+        int a=10;
+        int b=5;
+        System.out.println(a*b);
+    }
+    public static void main(String[] args) {
+        mul();
+    }
+}
