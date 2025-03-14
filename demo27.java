d3621abd3e5a8c86721942ca661e1bec62468c89
Pavithra-2705 <pavithramuthuraj343@gmail.com>
2025-03-14 13:49:03 +0530

    Array

---
+++ demo27.java
@@ -0,0 +1,9 @@
+public class demo27{
+    public static void main(String[] args) {
+        String [] colors={"Red","White","Blue","Pink","Green","Black","Violet","Orange"};
+        System.out.println(colors[2]);
+        System.out.println(colors[6]);
+        
+
+    }
+}
