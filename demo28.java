eec46cfe3bd2dad72bf6e7f3411b883eaf1e6bf4
Pavithra-2705 <pavithramuthuraj343@gmail.com>
2025-03-14 14:20:04 +0530

    Two Dimensional Array

---
+++ demo28.java
@@ -0,0 +1,8 @@
+public class demo28{
+    public static void main(String[] args) {
+        int [][] num={{2,4,6},{1,3,5}};
+        System.out.println(num[1][2]);
+        String [][] colors={{"Red","Pink","Blue","Green","Violet"},{"Black","Brown","Yellow","Purple","Orange"}};
+        System.out.println(colors[0][1]);
+    }
+}
