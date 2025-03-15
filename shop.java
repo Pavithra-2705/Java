22f8dc05adb3290f5f74775f715852e316e21386
Pavithra-2705 <pavithramuthuraj343@gmail.com>
2025-03-15 11:01:30 +0530

    Shop

---
+++ shop.java
@@ -0,0 +1,11 @@
+public class shop{
+    public static void main(String[] args) {
+        int i;
+        String [] name={"Chocolates","Waffers","Cakes","Icecream","Cold drinks"};
+        int [] price={40,20,100,30,50};
+        for(i=0;i<=4;i++){
+            System.out.println(name[i]+"   \t:"+price[i]);
+        }
+
+    }
+}
