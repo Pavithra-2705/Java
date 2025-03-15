9729a4026469823bc49cbf3cf3034f77f03366b4
Pavithra-2705 <pavithramuthuraj343@gmail.com>
2025-03-15 10:38:42 +0530

    Billing System

---
+++ demo30.java
@@ -0,0 +1,10 @@
+public class demo30{
+    public static void main(String[] args) {
+        int i;
+        int[] a={101,102,103,105,106,107,108};
+        for(i=0;i<=5;i++){
+            System.out.println("Invoice No :"+a[i]);
+        }
+
+    }
+}
